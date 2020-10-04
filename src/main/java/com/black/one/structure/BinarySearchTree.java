3bf7ddd87d605ec84db77f1ad8c4cd47b0043cab
suwenhai <18250582119@163.com>
2020-10-04 22:54:55 +0800

    提交时间为2020年10月4号

---
+++ src/main/java/com/black/one/structure/BinarySearchTree.java
@@ -0,0 +1,24 @@
+package com.black.one.structure;
+
+/**
+ * 二叉树
+ *
+ * @author swh
+ * @create: 2020-10-03 21:23
+ */
+public class BinaryTree<E>{
+
+    int size(){}
+
+    boolean isEmpty(){}
+
+    void clear(){}
+
+    void add(E element){}
+
+    void remove(){}
+
+    boolean contains(E element){}
+
+
+}
